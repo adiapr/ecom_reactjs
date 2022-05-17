879580e3a6703672410e855e641309ec910b0250
adiapr <da801100@gmail.com>
2022-05-17 22:41:21 +0700

    Input asset di di index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        d
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button variant="warning">Warning</Button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
