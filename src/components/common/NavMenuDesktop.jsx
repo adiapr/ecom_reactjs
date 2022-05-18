f8ab16e989adb00372d573a73b4b2df4fa5cd6a7
adiapr <da801100@gmail.com>
2022-05-18 21:04:37 +0700

    install react router

---
+++ src/components/common/NavMenuDesktop.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class navMenuDesktop extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default navMenuDesktop
