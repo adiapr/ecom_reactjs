c314e600b79bed7c564f222a2fdc1033761bd4d7
adiapr <da801100@gmail.com>
2022-05-27 12:04:22 +0700

    Desktop Menu

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import {Navbar, Container, Row, Col, Button} from 'react-bootstrap'
+import Logo from '../../assets/images/easyshop.png'
+// memasukkan react router 
+import {
+  BrowserRouter as  Link
+} from "react-router-dom";
 
 export class navMenuDesktop extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Fragment>
+        <div className="TopSectionDown">
+          <Navbar fixed={"top"} className="navbar" bg="light">
+            <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
+              <Row>
+                
+                <Col lg={4} sm={12}>
+                  <Link to="/"><img className="nav-logo" src={Logo}></img></Link>
+                </Col>
+                
+                <Col lg={4} sm={12} className="p1 mt-1">
+                  <div className="input-group w-100">
+                    <input type="text" className="form-control" />
+                    <Button type="button" className="btn site-btn"><i className="fa fa-search"> </i> 
+                    </Button>
+                  </div>
+                </Col>
+                
+                <Col className="p1 mt-1" lg={4} sm={12}>
+                  <Link to="/" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>                  
+                   </Link>
+                   <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
+                   <Link to="/" className="h4 btn">LOGIN</Link>
+
+                   <Button className="cart-btn  pull-right"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
+                </Col>
+
+              </Row>
+            </Container>
+          </Navbar>
+        </div>
+      </Fragment>
     )
   }
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,17 +4,19 @@ import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
 import NewArrival from '../components/home/NewArrival'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
 
 export class HomePage extends Component {
   render() {
     return (
         <Fragment>
             {/* include file .jsx  */}
+            <NavMenuDesktop />
             <HomeTop />
             <FeaturedProducts />
             <NewArrival />
-            <Collection />
             <Categories />
+            <Collection />  
         </Fragment>
     )
   }
